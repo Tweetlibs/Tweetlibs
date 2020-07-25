de6d6591d299ecbb343a38dac7dfb362d2b592e6
Tyler Blakeman <tylerblakeman@gmail.com>
2020-07-24 22:14:54 -0500

    added models

---
+++ models/tweets.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+var TweetsSchema = new Schema ({
+  account_name: {
+    type: String,
+    require: true
+  },
+  user_name: {
+    type: String,
+    require: true
+  },
+  text: {
+    type: String,
+    require: true
+  },
+  image_url: {
+    type: String,
+    require: true
+  }
+})
