2ec57045623d754d6face4ff0140253a4fcdbac0
erkartali <erkartali@gmail.com>
2020-08-03 20:18:40 -0500

    updated front end look for main page and create page

--- client/src/components/pages/Main.js
+++ client/src/components/pages/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Libs from '../libs/Libs'
+import LoginBar from '../LoginBar'
 
 const Main = () => {
     return (
         <div>
-            Stuff
+            <LoginBar />
+            <h1>Checkout the most recent MovieLibs!</h1>
             <Libs />
         </div>
     );
